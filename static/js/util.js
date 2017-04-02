a65fbb70a243bc0cd24c01341002978271233c7a
liang <liul@eeparking.com>
2017-04-02 10:39:45 +0800

    sdfsdf9

--- static/js/util.js
+++ static/js/util.js
@@ -36,7 +36,9 @@
       _load = option.addStatus!=undefined?option.addStatus:"加载中...";
       _more = option.stopStatus!=undefined?option.stopStatus:"上拉加载";
       _over = option.overStatus!=undefined?option.overStatus:"没有更多";
+      _auto = option.autoLoad!=undefined&&option.autoLoad?true:false;
       _callback = option.callback!=undefined&&typeof(option.callback)=="function"?option.callback:null;
+
       if(_more!=""){
         var $loader = $("div.mui-content").find("p.ymx-more");
         if($loader.length==0){
@@ -53,27 +55,46 @@
           $("div.mui-content").append($more);
         }
       }
+      if(_auto&&_callback!=null){loadCallback($more,0);}
     }else{
       mui.toast("方法参数不合法！");
       return false;
     }
+    //滚动条事件
     $(window).scroll(function(e){
       var _win_height =$(window).height();
       var _scroll_height = $(document).scrollTop();
       var _cha =$(document).height() - _win_height;
       if(_scroll_height === _cha){
         if(_callback!=null){
-          var _text = $more.text();
-          if($more!=undefined&&_text===_more&&_text!=_load){
-            $more.addClass("loading");
-            $more.text(_load);
-            $more.loadPoint=function(){
-              $(document).scrollTop(_scroll_height);
-            }
-            _callback($more);
-          }
+          loadCallback($more,_scroll_height);
         }
       }
     });
+
+    //加载回调函数数据
+    function loadCallback($more,_scroll_height){
+      var _text = $more.text();
+      if($more!=undefined&&_text===_more&&_text!=_load){
+        $more.addClass("loading");
+        $more.text(_load);
+        $more.loadPoint=function(){
+          $(document).scrollTop(_scroll_height);
+        }
+        _callback($more);
+      }
+    }
+  }
+
+  /**
+   * 随机生成指定长度字符串
+   */
+  window.randomStr = function(len){
+    var _str ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var _sb ="";
+    for(var i=0;i<len;i++){
+      _sb+=_str.charAt(Math.ceil(Math.random()*(_str.length-1)));
+    }
+    return _sb;
   }
 })(jQuery);
