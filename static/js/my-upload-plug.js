0dd645a3dfcf1c758b70f77396d794aaff27c7e6
liang <liul@eeparking.com>
2017-04-09 03:28:07 +0800

    sdfsdf7

--- static/js/my-upload-plug.js
+++ static/js/my-upload-plug.js
@@ -29,6 +29,7 @@
         allowMagnify:false,
         crop:false
       },
+      // fileNumLimit:3,                //文件最多个数
       compress:false,
       auto:option.auto!=undefined&&typeof(option.auto)==="boolean"?option.auto:true, //是否自动上传
       chunked:option.chunked!=undefined&&typeof(option.chunked)==="boolean"?option.chunked:false, //是否开启分片上传
@@ -42,9 +43,9 @@
     });
 
     //当文件被加入队列之前触发，此事件的handler返回值为false，则此文件不会被添加进入队列。
-    $uploader.on("beforeFileQueued",function(file){
-      console.log("加入一个队列前");
-    });
+    // $uploader.on("beforeFileQueued",function(file){
+    //   console.log("加入一个队列前");
+    // });
 
 /**
     //当文件被加入队列以后触发
@@ -62,24 +63,24 @@
     */
 
     //当一批文件添加进队列以后触发
-    $uploader.on("filesQueued",function(files){
-      console.log("加入一批文件队列后");
-    });
+    // $uploader.on("filesQueued",function(files){
+    //   console.log("加入一批文件队列后");
+    // });
 
     //当文件被移除队列后触发
-    $uploader.on("fileDequeued",function(file){
-      console.log("移除一个文件队列后");
-    });
+    // $uploader.on("fileDequeued",function(file){
+    //   console.log("移除一个文件队列后");
+    // });
 
     //当所有文件上传结束时触发
-    $uploader.on("uploadFinished",function(){
-      console.log("所有文件上传结束");
-    });
+    // $uploader.on("uploadFinished",function(){
+    //   console.log("所有文件上传结束");
+    // });
 
     //某个文件开始上传前触发，一个文件只会触发一次。
-    $uploader.on("uploadStard",function(file){
-      console.log(file.name+"开始上传");
-    });
+    // $uploader.on("uploadStard",function(file){
+    //   console.log(file.name+"开始上传");
+    // });
 
     
     /**当某个文件的分块在发送前触发，主要用来询问是否要添加附带参数，大文件在开起分片上传的前提下此事件可能会触发多次。
@@ -87,43 +88,43 @@
      * data {Object}默认的上传参数，可以扩展此对象来控制上传参数。
      * headers {Object}可以扩展此对象来控制上传头部。
      */
-    $uploader.on("uploadBeforeSend",function(object,data,headers){
+    // $uploader.on("uploadBeforeSend",function(object,data,headers){
 
-    });
+    // });
 
     /**当某个文件上传到服务端响应后，会派送此事件来询问服务端响应是否有效。如果此事件handler返回值为false, 则此文件将派送server类型的uploadError事件。
      *object {Object}
      *ret {Object}服务端的返回数据，json格式，如果服务端不是json格式，从ret._raw中取数据，自行解析。
      */
-    $uploader.on("uploadAccept",function(object,ret){
+    // $uploader.on("uploadAccept",function(object,ret){
 
-    });
+    // });
 
     //上传进度 percentage{Number} 上传进度
-    $uploader.on("uploadProgress",function(file,percentage){
+    // $uploader.on("uploadProgress",function(file,percentage){
 
-    });
+    // });
 
     /**上传文件出错
      *file {File}File对象
      *reason {String}出错的code
      */
-    $uploader.on("uploadError",function(file,reason){
-      console.log(file.name+"文件上传出错");
-    });
+    // $uploader.on("uploadError",function(file,reason){
+    //   console.log(file.name+"文件上传出错");
+    // });
 
     /**当文件上传成功时触发
      *file {File}File对象
      *response {Object}服务端返回的数据
      */
-    $uploader.on("uploadSuccess",function(file,response){
-      console.log(file.name+"文件上传成功！");
-    });
+    // $uploader.on("uploadSuccess",function(file,response){
+    //   console.log(file.name+"文件上传成功！");
+    // });
 
     //不管成功或者失败，文件上传完成时触发。
-    $uploader.on("uploadComplete",function(file){
+    // $uploader.on("uploadComplete",function(file){
 
-    });
+    // });
     return $uploader;
   }
 })(jQuery,WebUploader);
